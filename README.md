72a99c5fa861cee56149cb58d808414b9b2c1760
Luis1218 <luigi-11@live.com.mx>
2021-02-15 18:09:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-yjvc5r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yjvc5r)
---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("hello world!!");
+console.log(30);
+console.log(true);
+console.log(3.1416);
+
+//comentario en consola
+
+// tipos de variables 
+// palabra reservada var 
+// variables numericas var 
+// 1. numericas: int, float 
+// 2. String: cadenas de caracteres
+// 3. Boolean: true or false
+// 4. Arrays: Coleccion de datos, conjunto de datos.
+// 5. Objects: coleciion de propiedades
+
+var age = 22; 
+console.log(age);
+var main = 'informatica' ;
+console.log (main);
+var status = false;
+console.log (status);
+var pets = ['dogs', 'cats', 'chickens'];
+console.log (pets);
